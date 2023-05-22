565239f0c4eb9763a8cd611c0798377f1e6ca42c
Andrey_Marusov <marusov140@gmail.com>
2023-05-22 13:33:23 +0300

    [22.05]-[13:33]:Работает регистрация и авторизация через jwt токен

--- frontend/src/baseComponents/Button/Button.jsx
+++ frontend/src/baseComponents/Button/Button.jsx
@@ -3,9 +3,19 @@ import classNames from "classnames";
 import styles from "./Button.module.scss";
 import arrow from "./img/arrowRight.svg";
 
-const Button = ({ value, className, classNameImg, type = "button" }) => {
+const Button = ({
+  value,
+  className,
+  classNameImg,
+  type = "button",
+  onClick,
+}) => {
   return (
-    <button type={type} className={classNames(styles.btn, className)}>
+    <button
+      type={type}
+      className={classNames(styles.btn, className)}
+      onClick={onClick}
+    >
       <span>{value}</span>
       <img src={arrow} alt="" className={classNameImg} />
     </button>
