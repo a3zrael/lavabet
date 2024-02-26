9df38f16318617e25e77e62dea96a44340e01a46
Andrey_Marusov <marusov140@gmail.com>
2024-02-27 00:08:17 +0300

    vercel deploy

--- src/components/Earn/Earn.data.js
+++ src/components/Earn/Earn.data.js
@@ -1,10 +1,11 @@
-import icon1 from "./img/firstBoximg.svg";
+import icon1 from "./img/icon1.svg";
 import icon2 from "./img/icon2.svg";
 import icon3 from "./img/icon3.svg";
 import icon4 from "./img/icon4.svg";
 
 export const array = [
-    { title: 'Direct advertiser', icon: icon1, },
-    { title: 'More than 5 converting geos', icon: icon2, },
-    { title: 'Direct advertiser', icon: icon3, },
-    { title: 'Direct advertiser', icon: icon4, }]
+  { title: "Direct advertiser", icon: icon1 },
+  { title: "More than 5 converting geos", icon: icon2 },
+  { title: "Direct advertiser", icon: icon3 },
+  { title: "Direct advertiser", icon: icon4 },
+];
